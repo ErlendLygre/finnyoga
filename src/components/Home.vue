8e915413a4fd0fb34652e1ad7ab265f7787afec7
= <hnytun@gmail.com>
2019-02-22 14:26:29 +0100

    finished homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="home">
-
-    <div class="textDiv">
-    <h1>Velkommen til finnYoga</h1>
-    <v-btn to="/dropins" large color="primary" round>Finn din neste yogatime</v-btn>
-
-    </div>
+      <div class="logoDiv">
+        <img src="../assets/logo_new.png">
+      </div>
+      <div class="btnDiv">
+        <v-btn to="/dropins" large color="success" round>Finn din neste yogatime</v-btn>
+      </div>
   </div>
 </template>
 
@@ -17,20 +17,30 @@
 <style type="text/css">
 
 .home {
-  background-image: url("../assets/yogastudio2.jpg");
+  
   height: 100%;
   width: 100%;
   text-align: center;
+
+  background: url("../assets/yogastudio2.jpg") no-repeat center center fixed; 
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
   
   
 }
 
-.textDiv {
+.logoDiv {
+  margin-right:-6.5%;
+}
 
+.btnDiv {
   padding-top:15%;
   justify-content: center;
   bottom: 100;
-  margin-bottom:50%;
+  margin-top:-20%;
+  margin-right:0%;
   font-family:Arial, Helvetica, sans-serif;
   opacity: 0.8;
 }
