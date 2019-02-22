2bc7f7962f56457aa2cde405753c3cf47ccfb0cc
= <hnytun@gmail.com>
2019-02-22 13:54:03 +0100

    made homepage prettier

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,42 @@
 <template>
   <div class="home">
-    Velkommen til finnyoga, verdens beste app, laget av henrik og erlend
+
+    <div class="textDiv">
+    <h1>Velkommen til finnYoga</h1>
     <v-btn to="/dropins" large color="primary" round>Finn din neste yogatime</v-btn>
+
+    </div>
   </div>
 </template>
 
 <script>
 </script>
+
+
+
+<style type="text/css">
+
+.home {
+  background-image: url("../assets/yogastudio2.jpg");
+  height: 100%;
+  width: 100%;
+  text-align: center;
+  
+  
+}
+
+.textDiv {
+
+  padding-top:15%;
+  justify-content: center;
+  bottom: 100;
+  margin-bottom:50%;
+  font-family:Arial, Helvetica, sans-serif;
+  opacity: 0.8;
+}
+
+
+
+</style>
+
+
